40ee51cd971bc6c839e7a8d9c634edd3666118c8
Darren Joseph <thedarrenjoseph@gmail.com>
2021-09-09 23:34:22 +0100

    Refactor initial menu drawing code

--- arq/src/main.rs
+++ arq/src/main.rs
@@ -1,20 +1,34 @@
 use std::io;
-use tui::widgets::{Block, Borders};
+use tui::widgets::{Block, Borders, List, ListItem};
 use tui::style::{Style, Color};
 use tui::layout::{Rect};
+use std::convert::TryInto;
 
 mod terminal_manager;
 
-fn main<>() -> Result<(), io::Error> {
+fn draw_start_menu<B : tui::backend::Backend>(frame : &mut tui::terminal::Frame<'_, B> ) {
+    let frame_size = frame.size();
 
-    let mut terminal_manager = terminal_manager::init::<tui::backend::TermionBackend<termion::raw::RawTerminal<std::io::Stdout>>>().unwrap();
-    return terminal_manager.terminal.draw(|frame| {
-        let frame_size = frame.size();
-        let size = Rect::new(frame_size.x, frame_size.y, frame_size.width-2, frame_size.height-2);
-        let block = Block::default()
-            .title("ARQ")
-            .borders(Borders::ALL)
-            .style(Style::default().bg(Color::Black));
-        frame.render_widget(block, size)
-    });
+    let main_block = Block::default()
+        .borders(Borders::ALL)
+        .style(Style::default().bg(Color::Black));
+    let main_size = Rect::new(frame_size.x, frame_size.y, frame_size.width-2, frame_size.height-2);
+    frame.render_widget(main_block, main_size);
+
+    let menu_titles = ["Play", "Settings", "Info", "Quit"];
+    let menu_items : Vec<ListItem> = menu_titles.iter().cloned().map(ListItem::new).collect();
+    let menu_list = List::new(menu_items)
+        .block(Block::default()
+            .borders(Borders::NONE)
+        )
+        .style(Style::default().fg(Color::White))
+        .highlight_style(Style::default().fg(Color::Yellow));
+    let menu_size = Rect::new(4, 4, main_size.width/2, menu_titles.len().try_into().unwrap());
+    frame.render_widget(menu_list, menu_size);
+
+}
+
+fn main<>() -> Result<(), io::Error> {
+    let mut terminal_manager = terminal_manager::init().unwrap();
+    return terminal_manager.terminal.draw(|frame| { draw_start_menu(frame) });
 }
--- arq/src/terminal_manager.rs
+++ arq/src/terminal_manager.rs
@@ -8,7 +8,7 @@ pub struct TerminalManager<B : tui::backend::Backend> {
     pub terminal : tui::Terminal<B>
 }
 
-pub fn init<W : termion::raw::IntoRawMode>() -> Result<TerminalManager<TermionBackend<RawTerminal<io::Stdout>>>, io::Error> {
+pub fn init() -> Result<TerminalManager<TermionBackend<RawTerminal<io::Stdout>>>, io::Error> {
     let stdout = io::stdout().into_raw_mode()?;
     let backend = TermionBackend::new(stdout);
     let terminal = Terminal::new(backend)?;
