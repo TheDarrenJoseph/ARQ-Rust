81d262a74eca6772aab76f0dc0db6ff15ca28994
Darren Joseph <thedarrenjoseph@gmail.com>
2021-09-10 23:32:54 +0100

    Add log4rs w/ file logging. Initial blocking stdin capture example

--- arq/src/main.rs
+++ arq/src/main.rs
@@ -4,6 +4,10 @@ mod terminal_manager;
 mod ui;
 
 fn main<>() -> Result<(), io::Error> {
-    let mut terminal_manager = terminal_manager::init().unwrap();
-    return terminal_manager.terminal.draw(|frame| { ui::draw_start_menu(frame) });
+    log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
+    let mut manager = terminal_manager::init().unwrap();
+    manager.terminal.clear()?;
+    manager.terminal.draw(|frame| { ui::draw_start_menu(frame) });
+    terminal_manager::get_input_key();
+    return Ok(());
 }
--- arq/src/terminal_manager.rs
+++ arq/src/terminal_manager.rs
@@ -1,18 +1,39 @@
 use std::io;
 use tui::Terminal;
 use tui::backend::TermionBackend;
-use termion::raw::IntoRawMode;
-use termion::raw::RawTerminal;
+use termion::raw::{IntoRawMode, RawTerminal};
+use termion::input::TermRead;
+use termion::{event::Key};
 
 pub struct TerminalManager<B : tui::backend::Backend> {
-    pub terminal : tui::Terminal<B>
+    pub terminal : tui::Terminal<B>,
+}
+
+pub fn get_input_key() {
+    loop {
+        //log::info!("Loop {}", n);
+        let input = io::stdin();
+        let keys = input.keys();
+        for key in keys {
+            match key.unwrap() {
+                Key::Char('q') => {
+                    log::info!("Quitting...");
+                    return;
+                }
+                Key::Char(c) => {
+                    log::info!("Inputted: '{}'", c);
+                }
+                _ => ()
+            }
+        }
+    }
 }
 
 pub fn init() -> Result<TerminalManager<TermionBackend<RawTerminal<io::Stdout>>>, io::Error> {
     let stdout = io::stdout().into_raw_mode()?;
     let backend = TermionBackend::new(stdout);
     let terminal = Terminal::new(backend)?;
-
     let manager = TerminalManager { terminal };
+    log::info!("Terminal initialised.");
     return Ok(manager);
 }
