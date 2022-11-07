43a46b963d54a271164084490ffcfcd20a3fc287
Darren Joseph <thedarrenjoseph@gmail.com>
2022-11-07 13:56:12 +0000

    Fix read/write access for background music sink

--- arq/src/sound/sound.rs
+++ arq/src/sound/sound.rs
@@ -1,6 +1,6 @@
 use std::fs::File;
 use std::io::BufReader;
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, RwLock};
 use std::thread;
 use std::thread::JoinHandle;
 use std::time::Duration;
@@ -8,13 +8,12 @@ use log::log;
 use rodio::{Decoder, OutputStream, source::Source, OutputStreamHandle, Sink, PlayError};
 
 pub struct SoundSinks {
-    //pub bg_source: Option<Source<Decoder<R>>>,
-    //pub bg_sink: Option<Arc<Mutex<Sink>>>,
-    //pub bg_thread: Option<thread::JoinHandle<()>>
+    os : Option<Arc<OutputStream>>,
+    pub bg_sink: Option<Arc<RwLock<Sink>>>,
 }
 
 pub fn build_sound_sinks() -> SoundSinks {
-    let mut sinks = SoundSinks {}; // bg_sink: None, bg_thread: None };
+    let mut sinks = SoundSinks { os:None, bg_sink: None }; // bg_sink: None, bg_thread: None };
     sinks.setup_background_music();
     return sinks;
 }
@@ -23,35 +22,36 @@ impl SoundSinks {
 
     pub fn setup_background_music(&mut self) {
         log::info!("Starting background music..");
+        let (stream, stream_handle) = OutputStream::try_default().unwrap();
+        self.os = Some(Arc::new(stream));
+        let sink_arc = Arc::new(RwLock::new(Sink::try_new(&stream_handle).unwrap()));
+        self.bg_sink = Some(sink_arc.clone());
 
-        let handle = thread::spawn(move || {
-            let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-            let sink = Sink::try_new(&stream_handle).unwrap();
-
+        thread::spawn(move || {
+            let stream_handle = stream_handle;
             let file = BufReader::new(File::open("resources/alexander-nakarada-tavern-loop-one.mp3").unwrap());
-            let source = Decoder::new(file).unwrap();
-            sink.append(source);
+            let looped_decoder = Decoder::new_looped(file).unwrap();
 
+            let sink = sink_arc.write().unwrap();
+            sink.append(looped_decoder);
             sink.play();
-
-            loop {
-                log::debug!("Pausing background music thread..");
-                // Give 900ms back to the main thread
-                thread::sleep(Duration::from_millis(900));
-            }
         });
-
-        //let arc = Arc::new(Mutex::new(sink));
-        //self.bg_sink = Some(arc);
     }
 
     pub fn play_background(&mut self) {
-        // TODO test accessing Sink volume etc/thread pausing
-        //let inner_arc = Arc::clone(&self.bg_sink.as_ref().unwrap());
-        //let sink = inner_arc.lock().unwrap();
+        if let Some(sink) = &self.bg_sink {
+            let writeable = sink.write();
+            if let Ok(w) = writeable{
+                log::info!("Handling background music...");
+                // TODO pass a sound config into here
+                w.set_volume(0.5);
+            } else {
+                log::info!("No write lock.");
+            }
 
-        // Give 100ms to the background music thread
-        thread::sleep(Duration::from_millis(100));
+        } else {
+            log::info!("No bg sink to manage.");
+        }
     }
 }
 
