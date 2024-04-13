a34cd1e107df328b8ccbfba698c9b97aefaf1569
Darren Joseph <thedarrenjoseph@gmail.com>
2024-04-13 18:47:05 +0100

    Fix background music thread looping

--- arq/src/sound/sound.rs
+++ arq/src/sound/sound.rs
@@ -2,16 +2,19 @@ use std::fs::{File, ReadDir};
 use std::io::{BufReader, Error, ErrorKind};
 use std::sync::{Arc, RwLock};
 use std::{fs, thread};
+use std::time::Duration;
 use log::{error, info};
 use rand::{Rng, SeedableRng};
 use rand::seq::IteratorRandom;
 use rand_pcg::Pcg64;
 use rand_seeder::Seeder;
 
-use rodio::{Decoder, OutputStream, Sink};
+use rodio::{Decoder, OutputStream, OutputStreamHandle, Sink};
 use termion::event::Key::F;
 use tokio::io::AsyncWriteExt;
 use tokio::runtime::Runtime;
+use tokio::task;
+use tokio::time::sleep;
 
 use crate::sound::audio_sink::AudioSink;
 
@@ -47,30 +50,39 @@ pub fn pick_background_track(paths: &mut ReadDir, rng: &mut impl Rng) -> Result<
     }
 }
 
+pub async fn handle_background_music(sink_arc: Arc<RwLock<Sink>>) {
+    loop {
+        if sink_arc.write().unwrap().empty() {
+            let mut paths = fs::read_dir(RESOURCE_MUSIC_BACKGROUND_FOLDER).unwrap();
+            let mut rng = Pcg64::from_entropy();
+            match pick_background_track(&mut paths, &mut rng) {
+                Ok(track) => {
+                    let decoder = Decoder::new_mp3(track).unwrap();
+                    sink_arc.write().unwrap().append(decoder);
+                },
+                Err(e) => {
+                    error!("Failed to pick background track: {}", e)
+                }
+            }
+        } else {
+            info!("Background audio sink is not empty. Waiting 60s..");
+            sleep(Duration::from_secs(60)).await;
+        }
+    }
+}
+
+
 impl SoundSinks {
+
     pub fn setup_background_music(&mut self) {
         log::info!("Starting background music..");
         let (stream, stream_handle) = OutputStream::try_default().unwrap();
         self.bg_sink.set_os(Some(Arc::new(stream)));
         let sink_arc = Arc::new(RwLock::new(Sink::try_new(&stream_handle).unwrap()));
         self.bg_sink.set_sink(Some(sink_arc.clone()));
-
-        thread::spawn(move || {
-            let _stream_handle = stream_handle;
-            let sink = sink_arc.write().unwrap();
-            if sink.empty() {
-                let mut paths = fs::read_dir(RESOURCE_MUSIC_BACKGROUND_FOLDER).unwrap();
-                let mut rng = Pcg64::from_entropy();
-                match pick_background_track(&mut paths, &mut rng) {
-                    Ok(track) => {
-                        let decoder = Decoder::new_mp3(track).unwrap();
-                        sink.append(decoder);
-                    },
-                    Err(e) => {
-                        error!("Failed to pick background track: {}", e)
-                    }
-                }
-            }
+        
+        task::spawn(async move {
+            handle_background_music(sink_arc).await
         });
     }
 
